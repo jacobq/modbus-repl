93439f26dc0444b08080f6551aa5ef674d90c9c6
Michal Molawka <michal.molawka@sonovum.de>
2020-01-13 16:21:16 +0100

    added description of CLI args

--- src/config.js
+++ src/config.js
@@ -2,31 +2,31 @@ module.exports = require('yargs')
 .options({
 	'host': {
 		default: 'localhost',
-		describe: '',
+		describe: 'Address of the host running modbus server',
 		type: 'string',
 		alias: 'h',
 	},
 	'port': {
 		default: 502,
-		describe: '',
+		describe: 'Port on which modbus server is listening',
 		type: 'number',
 		alias: 'p',
 	},
 	'proto': {
 		default: 'tcp',
-		describe: '',
+		describe: 'Protocol used for modbus communication, valid options are `tcp` and `udp`',
 		type: 'string',
 		alias: 'P',
 	},
 	'headless': {
 		default: false,
-		describe: '',
+		describe: 'If set, given command will be executed affter connection is established, it\'s result will be printed to stdout and afterwards process will exit',
 		type: 'boolean',
 		alias: 'H',
 	},
 	'command': {
 		default: null,
-		describe: '',
+		describe: 'Command to be executed in headless mode',
 		type: 'string',
 		alias: 'c',
 	},
